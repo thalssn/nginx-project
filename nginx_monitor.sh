e4354b3b41ec58b741517f9535eef1c0fcfe6a9b
Thalisson <thalissonvini@outlook.com>
2024-10-10 19:25:48 +0000

    Corrigido erro no caminho dos diretorios de log

--- nginx_monitor.sh
+++ nginx_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_DIR=~/thalisson/nginx_project/nginx_logs
+LOG_DIR=~/nginx_project/logs
 
 if systemctl is-active --quiet nginx; then
 	STATUS_MESSAGE="$(date '+%d-%M-%Y %H:%M:%S') O serviço nginx está em execução - nginx - ONLINE"
