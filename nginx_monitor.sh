79a0e2b98c76546cfb5d6e109e3024fd7abb3e9f
Thalisson <thalissonvini@outlook.com>
2024-10-10 18:50:35 -0300

    Corrigido erro no parametro de minutos

--- nginx_monitor.sh
+++ nginx_monitor.sh
@@ -3,7 +3,7 @@
 LOG_DIR=~/nginx_project/logs
 
 if systemctl is-active --quiet nginx; then
-	STATUS_MESSAGE="$(date '+%d-%M-%Y %H:%M:%S') O serviço nginx está em execução - nginx - ONLINE"
+	STATUS_MESSAGE="$(date '+%d-%m-%Y %H:%M:%S') O serviço nginx está em execução - nginx - ONLINE"
 	echo "$STATUS_MESSAGE" >> "$LOG_DIR/nginx_online.log"
 	echo "$STATUS_MESSAGE" >> "$LOG_DIR/nginx_status.log"
 else
