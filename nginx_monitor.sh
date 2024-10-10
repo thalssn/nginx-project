4e632042b56ab93248278fc19fd28fb392076fe5
Thalisson <thalissonvini@outlook.com>
2024-10-10 19:07:31 +0000

    Script de validação criado

---
+++ nginx_monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LOG_DIR=~/thalisson/nginx_project/nginx_logs
+
+if systemctl is-active --quiet nginx; then
+	STATUS_MESSAGE="$(date '+%d-%M-%Y %H:%M:%S') O serviço nginx está em execução - nginx - ONLINE"
+	echo "$STATUS_MESSAGE" >> "$LOG_DIR/nginx_online.log"
+	echo "$STATUS_MESSAGE" >> "$LOG_DIR/nginx_status.log"
+else
+	STATUS_MESSAGE="$(date '+%d-%m-%Y %H:%M:%S') O serviço nginx não está em execução -  nginx - OFFLINE"
+	echo "$STATUS_MESSAGE" >> "$LOG_DIR/nginx_offline.log"
+	echo "$STATUS MESSAGE" >> "$LOG_DIR/nginx_status.log"
+fi
